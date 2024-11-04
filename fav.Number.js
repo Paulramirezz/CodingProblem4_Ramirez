b2db3cc12cd6204bc3be8a7ba8c3ed9c95217c32
Paulramirezz <ramirezjohnpaul747@gmail.com>
2024-11-04 19:51:44 +0800

    Create fav.Number.js

---
+++ fav.Number.js
@@ -0,0 +1,16 @@
+You sent
+let favNumber = Math.floor(Math.random() * 50) + 1; 
+
+let guess;
+while (guess !== favNumber) {
+    guess = parseInt(prompt("Guess the favorite number."));
+
+    if (guess < favNumber) {
+        alert("Too low, try again.");
+    } else if (guess > favNumber){
+        alert("Too high, try again.")
+    } else {
+        console.log("Correct, you guessed the favorite number.");
+        break;
+    }
+}
